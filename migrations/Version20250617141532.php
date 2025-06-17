2910e967b42970080a23ae9e1364225a4ba96508
BeKibo <Nguyen.davpro@gmail.com>
2025-06-17 16:26:30 +0200

    maj fixtture salle

--- migrations/Version20250617141532.php
+++ migrations/Version20250617141532.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250617140418 extends AbstractMigration
+final class Version20250617141532 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/SalleFixtures.php
+++ src/DataFixtures/SalleFixtures.php
@@ -13,7 +13,6 @@ class SalleFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
-
         $images = [
             'salle1.jpg',
             'salle2.jpg',
@@ -21,30 +20,39 @@ class SalleFixtures extends Fixture
             'salle4.jpg',
         ];
 
-        $room = [
+        $roomNames = [
             'Roberts',
             'Duponts',
             'Montess',
             'Charlemagne',
         ];
 
+        $nameCounter = [];
+
         for ($i = 0; $i < 50; $i++) {
+
+            // Choix d’un nom de base aléatoire
+            $base = $faker->randomElement($roomNames);
+
+            // Incrémentation du compteur
+            if (!isset($nameCounter[$base])) {
+                $nameCounter[$base] = 1;
+            } else {
+                $nameCounter[$base]++;
+            }
+
+            // Génération du nom complet
+            $uniqueName = $base . ' ' . $nameCounter[$base];
 
             $salle = new Salle();
             $salle
-                ->setNom($faker->company())
+                ->setNom($uniqueName)
                 ->setLieu($faker->address())
                 ->setCapacite($faker->randomElement([50, 80, 100, 140, 200]))
-                ->setImage('/medias/images/' . $faker->randomElement($images));
-
-
-            
-            $reserved = $faker->boolean(30);
-            $salle->setReserved($reserved);
-
+                ->setImage('/medias/images/' . $faker->randomElement($images))
+                ->setReserved($faker->boolean(30));
 
             $manager->persist($salle);
-
         }
 
         $manager->flush();
