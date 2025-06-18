737f2f2f9f4fab3414541c8f62b78a492bdc47b2
BeKibo <Nguyen.davpro@gmail.com>
2025-06-18 11:37:11 +0200

    update de M:Mig

--- migrations/Version20250618093137.php
+++ migrations/Version20250618093137.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250618091111 extends AbstractMigration
+final class Version20250618093137 extends AbstractMigration
 {
     public function getDescription(): string
     {
